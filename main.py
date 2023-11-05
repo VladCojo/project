e6b8cf532874652b7db0c8fe2c6e1edaab482b89
VladCojo <vladvcojocaru@gmail.com>
2023-11-05 08:46:10 +0200

    Update main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+def SolveNQueens(n):
+    
+    def is_valid_state(state):
+        pass
+    
+    def get_candidates(state):
+        pass 
+    
+    def search(state, solutions):
+        pass
+    
+    def solve():
+        pass
