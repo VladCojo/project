2e54edd02283ce4f54194f5217e4fcc3fa557962
VladCojo <vladvcojocaru@gmail.com>
2023-11-05 09:08:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,22 +7,36 @@ Q . . .
 """
 class Solution():
    
-    #SOLVE
+    
     def SolveNQueens(self, n):
         solutions = []
         state = []
         self.search(state, solutions, n)
         return solutions
     
-    #IS VALID STATE
+    
     def is_valid_state(self, state, n):
+        # check if it is a valid solution
         return len(state) == n
     
-    #GET CANDIDATES
-    def get_candidates(self, state, n):
-        pass 
     
-    #SEARCH
+    def get_candidates(self, state, n):
+        if not state:
+            return range(n) 
+        
+        # find the next position in state to populate
+        position = len(state)
+        candidates = set(range(n))
+        
+        # prune downd candidates that place the queen into attacks
+        for row, col in enumerate(state):
+            # discard the column index if it's occupied by a queen
+            candidates.discard(col)
+            dist = position - row
+            # discard diagonals
+            candidates.discard(col + dist)  
+            candidates.discard(col - dist)
+    
     def search(self, state, solutions, n):
         pass
     
