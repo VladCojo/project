f569924fbd17bb8c3c3389dde8438fe0104be00c
VladCojo <vladvcojocaru@gmail.com>
2023-11-10 09:05:37 +0200

    Update Solve_N_Queens.py

--- Solve_N_Queens.py
+++ Solve_N_Queens.py
@@ -18,15 +18,27 @@ class Solution():
         res = []
         board = [["."]*n for i in range(n)]
         
-    def backtrack(r):
-        if r == n:
-            copy = ["".join(row) for row in board]
-            res.append(copy)
-            return
-          
-          
-          
-          
+        def backtrack(r):
+            if r == n:
+                copy = ["".join(row) for row in board]
+                res.append(copy)
+                return
+            
+            for c in range(n):
+                if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                    continue
+                
+                col.add(c) 
+                posDiag.add(r + c)   
+                negDiag.add(r - c)
+                board[r][c] = "Q"
+                
+                backtrack(r+1)
+                
+                col.remove(c) 
+                posDiag.remove(r + c)   
+                negDiag.remove(r - c)
+                board[r][c] = "."
           
             
 opt = 0 
